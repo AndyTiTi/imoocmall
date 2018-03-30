f890bebcfdd7035f5c58f1c072abce156af86b94
andy <andy.fan@inforun.net>
2018-03-30 13:11:03 +0800

    开发公用模态框组件

--- src/views/GoodsList.vue
+++ src/views/GoodsList.vue
@@ -9,7 +9,7 @@
         <div class="filter-nav">
           <span class="sortby">Sort by:</span>
           <a href="javascript:void(0)" class="default cur">Default</a>
-          <a href="javascript:void(0)" class="price" @click="sortGoods">Price
+          <a href="javascript:void(0)" class="price" @click="sortGoods" :class="{'sort-up':!sortFlag}">Price
             <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-arrow-short" :class="{'sort-up': !sortFlag}" fill="black">
               <use xlink:href="#icon-arrow-short"></use>
             </svg>
@@ -78,6 +78,26 @@
       </div>
     </div>
     <div :class="{'md-overlay':showFilter}" @click="showFilter=false"></div>
+    <modal :mdShow="mdShow" @close="closeModal">
+      <p slot="message">
+        请先登录，否则无法加入到购物车
+      </p>
+      <div slot="btnGroup">
+        <a class="btn btn--m" href="javascript:;" @click="mdShow=false">关闭</a>
+      </div>
+    </modal>
+    <modal :mdShow="mdShowCart" @close="closeModal">
+      <p slot="message">
+        <svg class="icon-status-ok">
+          <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-status-ok"></use>
+        </svg>
+        <span>加入购物车成功！</span>
+      </p>
+      <div slot="btnGroup">
+        <a class="btn btn--m" href="javascript:;" @click="mdShowCart=false">继续购物</a>
+        <router-link class="btn btn--m" href="javascript:;" to="/cart">查看购物车</router-link>
+      </div>
+    </modal>
     <nav-footer></nav-footer>
   </div>
 </template>
@@ -87,6 +107,7 @@
   import './../assets/css/product.css'
   import NavHeader from '@/components/Header.vue'
   import NavBread from '@/components/Bread.vue'
+  import Modal from '@/components/Modal.vue'
   import NavFooter from '@/components/Footer.vue'
   import axios from 'axios'
 
@@ -108,7 +129,9 @@
         page: 1,
         pageSize: 8,
         busy:true,
-        loading:false
+        loading:false,
+        mdShow:false,
+        mdShowCart:false
       }
     },
     mounted: function () {
@@ -134,9 +157,9 @@
         }).then((result)=> {
           var res=result.data;
           if(res.status=='0'){
-            alert('加入成功！')
+            this.mdShowCart = true;
           }else{
-            alert(res.msg);
+            this.mdShow = true;
           }
         })
       },
@@ -179,12 +202,17 @@
           this.page++;
           this.getGoodsList(true);
         },500)
+      },
+      closeModal(){
+        this.mdShow = false;
+        this.mdShowCart = false;
       }
     },
     components: {
       NavHeader,
       NavBread,
-      NavFooter
+      NavFooter,
+      Modal
     }
   }
 </script>
@@ -193,5 +221,9 @@
     height: 100px;
     line-height: 100px;
     text-align: center;
+  }
+  .sort-up{
+    transform: rotate(180deg);
+    transition: all .3s ease-out;
   }
 </style>
